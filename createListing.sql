9fe0ea36023a9ba6fe386edf1f6242ce171cd71b
Liam <43306264+LiamNgo24@users.noreply.github.com>
2023-11-29 05:52:58 +0700

    merged update listing and publish listing to createListing

--- createListing.sql
+++ createListing.sql
@@ -9,3 +9,38 @@ BEGIN
     WHERE PropertyID = p_PropertyID;
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE UpdateListing(IN p_ListingID INT, IN p_Description TEXT, IN p_LeaseTerms TEXT, IN p_RentalPrice DECIMAL)
+BEGIN
+    UPDATE Listings 
+    SET ListingDescription = p_Description, LeaseTerms = p_LeaseTerms
+    WHERE ListingID = p_ListingID;
+
+    UPDATE Properties 
+    SET RentalPrice = p_RentalPrice
+    WHERE PropertyID = (SELECT PropertyID FROM Listings WHERE ListingID = p_ListingID);
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE PublishListing(IN p_ListingID INT)
+BEGIN
+    UPDATE Listings 
+    SET PublishedDate = CURDATE()
+    WHERE ListingID = p_ListingID;
+
+    UPDATE Properties 
+    SET ListingStatus = 'Listed'
+    WHERE PropertyID = (SELECT PropertyID FROM Listings WHERE ListingID = p_ListingID);
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE UnpublishListing(IN p_ListingID INT)
+BEGIN
+    UPDATE Properties 
+    SET ListingStatus = 'Not Listed'
+    WHERE PropertyID = (SELECT PropertyID FROM Listings WHERE ListingID = p_ListingID);
+END $$
+DELIMITER ;
